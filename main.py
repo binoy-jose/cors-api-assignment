7852ab777448e01659481559a5c260613b80c01e
Binoy Jose <25ds1000066@ds.study.iitm.ac.in>
2026-07-03 13:31:48 +0530

    Added JWT verification endpoint

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import time
 import uuid
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException
+from pydantic import BaseModel
+import jwt
 
 app = FastAPI()
 
@@ -12,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[ALLOWED_ORIGIN],
     allow_credentials=False,
-    allow_methods=["GET", "OPTIONS"],
+    allow_methods=["GET", "POST","OPTIONS"],
     allow_headers=["*"],
 )
 
@@ -30,6 +34,23 @@ async def add_custom_headers(request, call_next):
 
     return response
 
+PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----
+"""
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-r393h5hh.apps.exam.local"
+
+class TokenRequest(BaseModel):
+    token: str
+
 
 @app.get("/stats")
 def get_stats(values: str):
@@ -43,3 +64,28 @@ def get_stats(values: str):
         "max": max(numbers),
         "mean": sum(numbers) / len(numbers),
     }
+
+
+@app.post("/verify")
+def verify_token(data: TokenRequest):
+    try:
+        payload = jwt.decode(
+            data.token,
+            PUBLIC_KEY,
+            algorithms=["RS256"],
+            issuer=ISSUER,
+            audience=AUDIENCE,
+        )
+
+        return {
+            "valid": True,
+            "email": payload.get("email"),
+            "sub": payload.get("sub"),
+            "aud": payload.get("aud"),
+        }
+
+    except jwt.PyJWTError:
+        return JSONResponse(
+            status_code=401,
+            content={"valid": False}
+        )
