964fd8e25f73d8ad14d5bd9e8b68d8dbd93bc0a7
Binoy Jose <25ds1000066@ds.study.iitm.ac.in>
2026-07-03 14:31:54 +0530

    Edited the Restore Q1 and Q2 service

--- main.py
+++ main.py
@@ -35,16 +35,15 @@ async def add_custom_headers(request, call_next):
 
 # ---------------- JWT ----------------
 
-PUBLIC_KEY = """
------BEGIN PUBLIC KEY-----
+PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
 WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
-SI6iyrYbKR0NEBSqq4XkadEjsCs4LlgniT7GlkL9Mce3b0wGLs9/7ZIXdQIDAQAB
------END PUBLIC KEY-----
-"""
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----"""
 
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-r393h5hh.apps.exam.local"
@@ -72,14 +71,13 @@ def get_stats(values: str):
 
 @app.post("/verify")
 def verify_token(data: TokenRequest):
-
     try:
         payload = jwt.decode(
             data.token,
             PUBLIC_KEY,
             algorithms=["RS256"],
-            issuer=ISSUER,
             audience=AUDIENCE,
+            issuer=ISSUER,
         )
 
         return {
@@ -89,6 +87,15 @@ def verify_token(data: TokenRequest):
             "aud": payload.get("aud"),
         }
 
+    except Exception as e:
+        return JSONResponse(
+            status_code=401,
+            content={
+                "valid": False,
+                "error": type(e).__name__,
+                "message": str(e)
+            }
+        )
     except jwt.PyJWTError:
         return JSONResponse(
             status_code=401,
